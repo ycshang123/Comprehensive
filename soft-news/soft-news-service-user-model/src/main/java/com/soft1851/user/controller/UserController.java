9797bd725d4adb0d03409d76b6785faf27272fe0
ycshang <351234359@qq.com>
2020-11-14 18:15:30 +0800

    Swagger2接口文档工具的使用

--- soft-news/soft-news-service-user-model/src/main/java/com/soft1851/user/controller/UserController.java
+++ soft-news/soft-news-service-user-model/src/main/java/com/soft1851/user/controller/UserController.java
@@ -1,17 +1,29 @@
 package com.soft1851.user.controller;
 
 import com.soft1851.api.controller.user.UserControllerApi;
+import com.soft1851.pojo.AppUser;
 import com.soft1851.result.GraceResult;
 import com.soft1851.user.mapper.AppUserMapper;
 import org.springframework.web.bind.annotation.RestController;
+import tk.mybatis.mapper.entity.Example;
 
 import javax.annotation.Resource;
 @RestController
 public class UserController implements UserControllerApi {
     @Resource
     private AppUserMapper appUserMapper;
+
     @Override
-    public Object getUsers() {
+    public Object getAllUsers() {
         return GraceResult.ok(appUserMapper.selectAll());
+    }
+
+    @Override
+    public GraceResult getUserInfo(String userId) {
+        Example userExample = new Example(AppUser.class);
+        Example.Criteria userCriteria = userExample.createCriteria();
+        userCriteria.andEqualTo("id",userId);
+        AppUser user = appUserMapper.selectOneByExample(userExample);
+        return GraceResult.ok(user);
     }
 }
