4cf0e56f11dd73fb99db0f056aeb3a25fd5b25a9
ycshang <351234359@qq.com>
2020-11-14 16:09:19 +0800

    配置logback日志与多环境profile

---
+++ soft-news/soft-news-service-user-model/src/main/java/com/soft1851/user/Application.java
@@ -0,0 +1,12 @@
+package com.soft1851.user;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
---
+++ soft-news/soft-news-service-user-model/src/main/java/com/soft1851/user/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.soft1851.user.controller;
+
+import com.soft1851.api.controller.user.HelloControllerApi;
+import com.soft1851.result.GraceResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class HelloController implements HelloControllerApi {
+    @Override
+    public Object hello() {
+        log.info("info:hello");
+        return GraceResult.ok("hello");
+    }
+}
