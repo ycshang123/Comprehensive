fa2692b64268c467a3f196ae86e4815e5569b7ee
ycshang <351234359@qq.com>
2020-11-23 19:01:27 +0800

    整合SpringBoot（GridFS）

---
+++ soft-news/soft-news-service-files/src/main/java/com/soft1851/files/GridFsConfig.java
@@ -0,0 +1,22 @@
+package com.soft1851.files;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.gridfs.GridFSBucket;
+import com.mongodb.client.gridfs.GridFSBuckets;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GridFsConfig {
+
+    @Value("${spring.data.mongodb.database}")
+    private String mongodb;
+
+    @Bean
+    public GridFSBucket gridFSBucket(MongoClient mongoClient) {
+        MongoDatabase database = mongoClient.getDatabase(mongodb);
+        return GridFSBuckets.create(database);
+    }
+}
